5b356a89179a9b9f8964d91e09c30740e440be2e
Marco-Nagy <>
2024-03-03 03:18:35 +0200

    create go chat app

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
-  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
